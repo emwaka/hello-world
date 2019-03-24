90946c72edd782c0c3969cbd89de73c126ffd039
emwaka <48068457+emwaka@users.noreply.github.com>
2019-03-24 20:37:12 +0100

    javascript practice stuff

---
+++ script.js
@@ -0,0 +1,11 @@
+console.log("siktrrbäh!!!!!");
+
+var firstName ='Emmanuel';
+var age = 29;
+var isMarried = false;
+
+
+console.log("His name is: " + firstName + " , he's " + age + " years old. Is he married? " + isMarried);
+
+
+console.log(firstName + " " + age);
